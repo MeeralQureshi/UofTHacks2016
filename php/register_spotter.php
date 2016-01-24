f77cda80cd3dda7fe4d19e5c10fb230fee910291
Ryan <ryanscfan@gmail.com>
2016-01-24 00:35:46 -0500

    removed extra database creation

--- php/register_spotter.php
+++ php/register_spotter.php
@@ -64,43 +64,6 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    
-    //=============Create Relations====================
-    
-
-    // Relation 'Missing': Missing Persons
-
-    $sql = "CREATE TABLE IF NOT EXISTS Missing (
-        MID int NOT NULL AUTO_INCREMENT,
-        TweetURL varchar(255) NOT NULL,
-        Longitude float NOT NULL,
-        Latitude float NOT NULL,
-        TimeCreated int,
-        PRIMARY KEY (MID)
-    )";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Table Missing created successfully";
-    } else {
-        echo "Error creating table: " . $conn->error;
-    }
-
-    // Relation 'Spotter': People looking out for missing persons
-    // Note: SID is phone number of the spotter
-
-    $sql = "CREATE TABLE IF NOT EXISTS Spotter (
-        SID int NOT NULL,
-        Longitude float NOT NULL,
-        Latitude float NOT NULL,
-        PRIMARY KEY (SID)
-    )";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Table Spotter created successfully";
-    } else {
-        echo "Error creating table: " . $conn->error;
-    }
-        
     function convAddr($addr) {
         $curl = curl_init();
         // Set some options - we are passing in a useragent too here
