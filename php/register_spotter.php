f5b0b9139881fdddaed76e947f28fd1804bfd314
Meeral Qureshi <meeralqureshi@hotmail.com>
2016-01-24 00:33:58 -0500

    Added back button

--- php/register_spotter.php
+++ php/register_spotter.php
@@ -1,8 +1,55 @@
-<html>
-    <body>
 <?php
     
     require "twilio-php-master/Services/Twilio.php";
+        
+    // ----------------------TWILIO------------------------------------
+    
+ 
+    // Step 2: set our AccountSid and AuthToken from www.twilio.com/user/account
+    $AccountSid = "ACdadfc90dba80efa5925569edcc7bb13b";
+    $AuthToken = "2f622c44c25755edc76f6da736feba45";
+ 
+    // Step 3: instantiate a new Twilio Rest Client
+    $client = new Services_Twilio($AccountSid, $AuthToken);
+
+
+
+    if(empty($_POST['name'])  || 
+       empty($_POST['phone']))
+    {
+        $errors .= "\n Error: all fields are required";
+    }
+
+    $name = $_POST['name']; 
+    $phone = $_POST['phone']; 
+ 
+    // Step 4: make an array of people we know, to send them a message. 
+    // Feel free to change/add your own phone number and name here.
+    $people = array(
+        $phone => $name,
+    );
+ 
+    // Step 5: Loop over all our friends. $number is a phone number above, and 
+    // $name is the name next to it
+    foreach ($people as $number => $name) {
+ 
+        $sms = $client->account->messages->sendMessage(
+ 
+        // Step 6: Change the 'From' number below to be a valid Twilio number 
+        // that you've purchased, or the (deprecated) Sandbox number
+            "289-796-0748", 
+ 
+            // the number we are sending to - Any phone number
+            $number,
+ 
+            // the sms body
+            "Hey $name, Welcome to Spotter."
+        );
+        // Display a confirmation message on the screen
+        $link_address = "index.html"
+        echo "Testing. Sent message to $name";
+        echo "<a href='".$link_address."'>Back to Spotter</a>";
+    }
         
         
     // ----------------------DATABASE------------------------------------
@@ -84,58 +131,19 @@
     } else {
         echo "Error adding spotter: " . $conn->error;
     }
-        
-// ----------------------TWILIO------------------------------------
-    
- 
-    // Step 2: set our AccountSid and AuthToken from www.twilio.com/user/account
-    $AccountSid = "ACdadfc90dba80efa5925569edcc7bb13b";
-    $AuthToken = "2f622c44c25755edc76f6da736feba45";
- 
-    // Step 3: instantiate a new Twilio Rest Client
-    $client = new Services_Twilio($AccountSid, $AuthToken);
-
-
-
-    if(empty($_POST['name'])  || 
-       empty($_POST['phone']))
-    {
-        $errors .= "\n Error: all fields are required";
-    }
-
-    $name = $_POST['name']; 
-    $phone = $_POST['phone']; 
- 
-    // Step 4: make an array of people we know, to send them a message. 
-    // Feel free to change/add your own phone number and name here.
-    $people = array(
-        $phone => $name,
-    );
- 
-    // Step 5: Loop over all our friends. $number is a phone number above, and 
-    // $name is the name next to it
-    foreach ($people as $number => $name) {
- 
-        $sms = $client->account->messages->sendMessage(
- 
-        // Step 6: Change the 'From' number below to be a valid Twilio number 
-        // that you've purchased, or the (deprecated) Sandbox number
-            "289-796-0748", 
- 
-            // the number we are sending to - Any phone number
-            $number,
- 
-            // the sms body
-            "Hey $name, Welcome to Spotter."
-        );
-        // Display a confirmation message on the screen
-        $link_address = "index.html"
-        echo "Sent message to $name";
-        echo "<a href='".$link_address."'>Back to Spotter</a>";
-    }
     
 ?>
-        
-    </body>
-    
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+    <title>Signup form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+</body>
 </html>
